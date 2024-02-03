17961f07e78a7120b48d8d4c44e0c0339132423e
Ngarotosto <carolvsreyesz@gmail.com>
2024-02-03 08:38:53 +0300

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember(){
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "React", "Node", "MongoDB"],
+        address: {
+            city: "New York",
+            zip: 10021
+        }
+    }
+    return member;
+}
